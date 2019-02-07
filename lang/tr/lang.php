6c2d75af0c3fe9773cda4cff3d031dda74e133eb
Mahmut Namli <mahmudnamli@gmail.com>
2019-02-07 18:17:20 +0300

    Added Turkish translation (#14)
    Credit to @mahony0

---
+++ lang/tr/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => 'Bildirim',
+        'description' => 'Bildirim Servisi',
+    ],
+    'notifications' => [
+        'menu_label' => 'Bildirim kuralları',
+        'menu_description' => 'Bildirimleri tetikleyen olayları ve eylemleri yönetin.',
+        'name' => 'İsim',
+        'code' => 'Kod',
+        'notification_rule' => 'Bildirim kuralı',
+        'add_notification_rule' => 'Bildirim kuralı ekle',
+    ],
+    'action' => [
+        'add_notification_action' => 'Bildirim eylemi ekle',
+    ],
+    'permissions' => [
+        'manage_notifications' => 'Bildirim yönetimi',
+    ],
+];
