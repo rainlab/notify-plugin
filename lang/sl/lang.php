cc5ce5aa1d52c5a487819b46b494494debda5435
Blaz Orazem <blaz@orazem.si>
2020-04-07 12:18:40 +0200

    Slovenian language added

---
+++ lang/sl/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'plugin'        => [
+        'name'        => 'Obvestila',
+        'description' => 'Storitve obveščanja',
+    ],
+    'notifications' => [
+        'menu_label'            => 'Nastavitve obveščanja',
+        'menu_description'      => 'Upravljanje dogodkov in dejanj, ki prožijo obvestila.',
+        'name'                  => 'Ime',
+        'code'                  => 'Koda',
+        'notification_rule'     => 'Pravilo obveščanja',
+        'add_notification_rule' => 'Dodaj pravilo obveščanja',
+    ],
+    'action'        => [
+        'add_notification_action' => 'Dodaj dejanje obveščanja',
+    ],
+    'permissions'   => [
+        'manage_notifications' => 'Upravljanje z nastavitvami obveščanja',
+    ],
+];
