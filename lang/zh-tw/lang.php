9a47e8caea5d844971a836227dbdb8dacf9361a8
LeoCantThinkofAName <i7851@hotmail.com>
2020-09-15 17:08:49 +0800

    create zh-tw translation file

---
+++ lang/zh-tw/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => '通知',
+        'description' => '通知系統',
+    ],
+    'notifications' => [
+        'menu_label' => '通知規則',
+        'menu_description' => '管理觸發通知的事件及動作。',
+        'name' => '名稱',
+        'code' => '代碼',
+        'notification_rule' => '通知規則',
+        'add_notification_rule' => '新增通知規則',
+    ],
+    'action' => [
+        'add_notification_action' => '新增通知動作',
+    ],
+    'permissions' => [
+        'manage_notifications' => '管理通知功能',
+    ],
+];
