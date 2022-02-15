cf5018f0c63c3c789830b7b24cbba4eba35d912d
武志伟 <89636889+ZhiweiWu0425@users.noreply.github.com>
2022-02-15 13:55:17 +0800

    Add Chinese lang
    Add Chinese lang

---
+++ lang/zh-cn/lang.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'plugin' => [
+        'name' => '通知',
+        'description' => '通知系统',
+    ],
+    'notifications' => [
+        'menu_label' => '通知规则',
+        'menu_description' => '管理触发通知的事件及动作。',
+        'name' => '名称',
+        'code' => '代码',
+        'notification_rule' => '通知规则',
+        'add_notification_rule' => '新增通知规则',
+    ],
+    'action' => [
+        'add_notification_action' => '新增通知动作',
+    ],
+    'permissions' => [
+        'manage_notifications' => '管理通知功能',
+    ],
+];
